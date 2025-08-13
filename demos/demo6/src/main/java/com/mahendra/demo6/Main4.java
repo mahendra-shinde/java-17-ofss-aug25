b4d9721c29843a3f1722c48ed13417ad0770e238
Mahendra shinde <MahendraShinde@synergetics-india.com>
2025-08-13 14:24:16 +0530

    Collection stream

--- demos/demo6/src/main/java/com/mahendra/demo6/Main4.java
+++ demos/demo6/src/main/java/com/mahendra/demo6/Main4.java
@@ -17,10 +17,22 @@ public class Main4 {
 
 		Collections.sort(account2, new SortByBalance());
 		
-		System.out.println("All the accounts sorted by their balance: ");
-		account2.forEach(Main4::print);
+		//System.out.println("All the accounts sorted by their balance: ");
+		//account2.forEach(Main4::print);
 		
-		Collections.sort(account2, (a,b) -> a.getBalance().compareTo(b.getBalance())  );
+		//Collections.sort(account2, (a,b) -> a.getBalance().compareTo(b.getBalance())  );
+		
+		account2.stream().filter( a ->  a.getBalance() > 100000D  ).forEach(Main4::print);
+		
+		//Method reference would work only if
+		// 1. Method is static and returns VOID
+		// 2. Method takes ONLY one argument who's type matches with Element-type of collection "account2" 
+		
+		for(Account a : account2) {
+			if(a.getBalance() > 100000D) {
+				Main4.print(a);
+			}
+		}
 	}
 	
 	static void print(Account a) {
